0c514b74496547ad88ea3ce9d8e3fb085f199374
Tim Düsterhus <tim@bastelstu.be>
2020-10-20 18:21:52 +0200

    Migrate to TypeScript

--- Makefile
+++ Makefile
@@ -1,22 +1,20 @@
-WCF_FILES = $(shell find files_wcf -type f |sed 's/^files_wcf/files_wcf.out/')
+WCF_FILES = $(shell find files_wcf -type f)
+TS_FILES = $(shell find ts/ -type f |sed 's/ts$$/js/g;s!^ts/!files_wcf/js/!')
 
 all: be.bastelstu.wcf.push.tar
 
 be.bastelstu.wcf.push.tar: files_wcf.tar *.xml LICENSE
 	tar cvf be.bastelstu.wcf.push.tar --numeric-owner --exclude-vcs -- files_wcf.tar *.xml LICENSE
 
-files_wcf.tar: $(WCF_FILES)
-	tar cvf files_wcf.tar --numeric-owner --exclude-vcs --exclude .babelrc --transform='s,^files_wcf.out/,,' -- $+
+files_wcf.tar: $(WCF_FILES) $(TS_FILES)
+	tar cvf files_wcf.tar --numeric-owner --exclude-vcs -- $+
 
-files_wcf.out/%.js: files_wcf/%.js
-	-@mkdir -p $$(dirname $@)
-	yarn run babel $< --out-file $@
 
-files_wcf.out/%: files_wcf/%
-	-@mkdir -p $$(dirname $@)
-	cp -a $< $@
+files_wcf/js/%.js: ts/%.ts
+	yarn run tsc
 
 clean:
+	-find files_wcf/js/ -iname '*.js' -delete
 	-rm -f files_wcf.tar
 	-rm -rf files_wcf.out
 
--- ts/Bastelstu.be/_Push.ts
+++ ts/Bastelstu.be/_Push.ts
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2012 - 2016, Tim Düsterhus
+ * Copyright (c) 2012 - 2020, Tim Düsterhus
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * - Redistributions of source code must retain the above copyright notice, this list
  *   of conditions and the following disclaimer.
- * 
+ *
  * - Redistributions in binary form must reproduce the above copyright notice, this
  *   list of conditions and the following disclaimer in the documentation and/or
  *   other materials provided with the distribution.
- * 
+ *
  * - Neither the name of wbbaddons nor the names of its contributors may be used to
  *   endorse or promote products derived from this software without specific prior
  *   written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,26 +28,22 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
  */
 
-define([ 'Bastelstu.be/core' ], function (core) {
-	"use strict";
+class Push {
+  onConnect(callback) {
+    return Promise.reject(new Error("Unsupported"));
+  }
 
-	class Push {
-		onConnect(callback) {
-			return core.Promise.reject(new Error('Unsupported'))
-		}
+  onDisconnect(callback) {
+    return Promise.reject(new Error("Unsupported"));
+  }
 
-		onDisconnect(callback) {
-			return core.Promise.reject(new Error('Unsupported'))
-		}
+  onMessage(message, callback) {
+    return Promise.reject(new Error("Unsupported"));
+  }
 
-		onMessage(message, callback) {
-			return core.Promise.reject(new Error('Unsupported'))
-		}
+  getFeatureFlags() {
+    return [];
+  }
+}
 
-		getFeatureFlags() {
-			return [ ]
-		}
-	}
-
-	return new Push()
-});
+export = new Push();
